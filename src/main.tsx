cb8ba7832358399559f0d2d20b865355d48b31e2
Leonardo Araújo Sarmento <lsarmento@findes.org.br>
2024-08-26 09:59:43 -0300

    test memory router
    fix(i18n): translation home file

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import { RouterProvider, createRouter } from '@tanstack/react-router';
+import { RouterProvider, createMemoryHistory, createRouter } from '@tanstack/react-router';
 
 // Import the generated route tree
 import { routeTree } from './routeTree.gen';
@@ -8,7 +8,11 @@ import { AuthProvider, useAuth } from '@services/hooks/auth';
 import './index.css';
 import { ThemeProvider } from '@services/providers/Theme-provider';
 import { queryClient, QueryProvider } from '@services/providers/QueryProvider';
-import './i18n/config'
+import './i18n/config';
+
+const memoryHistory = createMemoryHistory({
+  initialEntries: ['/'], // Pass your initial url
+});
 
 // Create a new router instance
 const router = createRouter({
@@ -21,6 +25,7 @@ const router = createRouter({
   // Since we're using React Query, we don't want loader calls to ever be stale
   // This will ensure that the loader is always called when the route is preloaded or visited
   defaultPreloadStaleTime: 0,
+  history: memoryHistory,
 });
 
 // Register the router instance for type safety
