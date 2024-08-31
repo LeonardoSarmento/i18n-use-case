4e305253284b16403b3ac841b958d890eb20cfb4
Leonardo Araujo Sarmento <leosarmento@ucl.br>
2024-08-31 18:48:04 -0300

    feat(): github repo page nav link

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,12 +3,22 @@ import { PortfolioNav } from './PortfolioNav';
 import { ModeToggle } from './Mode-toggle';
 import { SelectLanguage } from './SelectLanguage';
 import { useTranslation } from 'react-i18next';
+import { Github } from 'lucide-react';
+import { Button } from './ui/button';
 
 export function NavigationBar() {
   const { t } = useTranslation('NavigationBar');
   return (
-    <div className="flex w-full flex-wrap-reverse items-center justify-center gap-4 xl:space-x-7 p-2 sm:my-2 md:flex-nowrap xl:justify-end xl:gap-2">
-      <div className="flex w-full flex-wrap justify-around gap-3">
+    <div className="flex w-full flex-wrap-reverse items-center gap-4 p-2 sm:my-2 md:flex-nowrap xl:gap-2 xl:space-x-7">
+      <div className="md:flex w-full justify-center hidden md:order-first xl:w-1/3 xl:justify-start">
+        <a href="https://github.com/LeonardoSarmento/i18n-use-case" target="_blank" rel="noopener noreferrer">
+          <Button variant="ghost" className="gap-3">
+            <Github />
+            <p className="text-base underline underline-offset-4">i18n-use-case</p>
+          </Button>
+        </a>
+      </div>
+      <div className="flex w-full flex-wrap justify-around justify-items-center gap-3 xl:w-1/3">
         <Link to="/" className="[&.active]:font-bold">
           {t('home')}
         </Link>
@@ -19,9 +29,16 @@ export function NavigationBar() {
           {t('zod')}
         </Link>
       </div>
-      <div className="flex w-full justify-around gap-3 md:justify-end">
+      <div className="flex w-full justify-around gap-3 md:justify-end xl:w-1/3">
+        <div className="md:hidden">
+          <a href="https://github.com/LeonardoSarmento/i18n-use-case" target="_blank" rel="noopener noreferrer">
+            <Button variant="ghost" className="gap-3">
+              <Github />
+            </Button>
+          </a>
+        </div>
+        <PortfolioNav />
         <SelectLanguage />
-        <PortfolioNav className="md:order-first" />
         <ModeToggle className="justify-self-end" />
       </div>
     </div>
