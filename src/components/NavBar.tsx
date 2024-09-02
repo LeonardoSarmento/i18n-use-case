503a0ea2008d52da95a7c976bd8fcd423a12abcf
Leonardo Araújo Sarmento <lsarmento@findes.org.br>
2024-09-02 14:17:25 -0300

    chore(): home translation

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -10,13 +10,8 @@ export function NavigationBar() {
   const { t } = useTranslation('NavigationBar');
   return (
     <div className="flex w-full flex-wrap-reverse items-center gap-4 p-2 sm:my-2 md:flex-nowrap xl:gap-2 xl:space-x-7">
-      <div className="md:flex w-full justify-center hidden md:order-first xl:w-1/3 xl:justify-start">
-        <a href="https://github.com/LeonardoSarmento/i18n-use-case" target="_blank" rel="noopener noreferrer">
-          <Button variant="ghost" className="gap-3">
-            <Github />
-            <p className="text-base underline underline-offset-4">i18n-use-case</p>
-          </Button>
-        </a>
+      <div className="hidden w-full justify-center md:order-first md:flex xl:w-1/3 xl:justify-start">
+        <GitHubLink />
       </div>
       <div className="flex w-full flex-wrap justify-around justify-items-center gap-3 xl:w-1/3">
         <Link to="/" className="[&.active]:font-bold">
@@ -31,16 +26,23 @@ export function NavigationBar() {
       </div>
       <div className="flex w-full justify-around gap-3 md:justify-end xl:w-1/3">
         <div className="md:hidden">
-          <a href="https://github.com/LeonardoSarmento/i18n-use-case" target="_blank" rel="noopener noreferrer">
-            <Button variant="ghost" className="gap-3">
-              <Github />
-            </Button>
-          </a>
+          <GitHubLink />
         </div>
         <PortfolioNav />
         <SelectLanguage />
         <ModeToggle className="justify-self-end" />
       </div>
     </div>
+  );
+}
+
+function GitHubLink() {
+  return (
+    <a href="https://github.com/LeonardoSarmento/i18n-use-case" target="_blank" rel="noopener noreferrer">
+      <Button variant="ghost" className="gap-3">
+        <Github />
+        <p className="text-base underline underline-offset-4 max-md:hidden">i18n-use-case</p>
+      </Button>
+    </a>
   );
 }
